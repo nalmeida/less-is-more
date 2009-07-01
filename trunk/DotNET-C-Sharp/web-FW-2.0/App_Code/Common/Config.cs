5f006a2bc4aef393ad046cd1f8c032e98752f308
regisbsbittencourt <regisbsbittencourt@be60e662-3d4f-0410-86c1-07df980b295e>
2009-07-01 15:59:26 +0000

    MOD: agora temos variaveis no css. falta documentar e trocar todos var(root) por $root no css

--- trunk/DotNET-C-Sharp/web-FW-2.0/App_Code/Common/Config.cs
+++ trunk/DotNET-C-Sharp/web-FW-2.0/App_Code/Common/Config.cs
@@ -9,26 +9,30 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
-namespace Common{
-	/**
-	 * Common.Config
-	 * @since 08/07/2008
-	 * @version 1.0
-	 * @author Regis Bittencourt - rbittencourt@fbiz.com.br, Marcelo Miranda Carneiro - mcarneiro@gmail.com
-	 */
-	public static class Config{
-	    /**
-		 * Common.Config.Title
-		 * @return String the project title name
-		 * @usage
-				<code>
-					<%=Common.Config.Title%> //writes " - ##PAGE_TITLE##" on the page.
-				</code>
-		 */
-		public static string Title{
-	        get{
-	            return " - ##PAGE_TITLE##";
-	        }
-	    }
-	}
+namespace Common
+{
+    /**
+     * Common.Config
+     * @since 08/07/2008
+     * @version 1.0
+     * @author Regis Bittencourt - rbittencourt@fbiz.com.br, Marcelo Miranda Carneiro - mcarneiro@gmail.com
+     */
+    public static partial class Config
+    {
+        /**
+         * Common.Config.Title
+         * @return String the project title name
+         * @usage
+                <code>
+                    <%=Common.Config.Title%> //writes " - ##PAGE_TITLE##" on the page.
+                </code>
+         */
+        public static string Title
+        {
+            get
+            {
+                return " - ##PAGE_TITLE##";
+            }
+        }
+    }
 }
