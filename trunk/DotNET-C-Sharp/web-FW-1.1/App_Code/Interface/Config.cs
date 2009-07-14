7a9be6cb624538bc822cff5082a754cefad3d89c
regisbsbittencourt <regisbsbittencourt@be60e662-3d4f-0410-86c1-07df980b295e>
2009-07-14 16:43:01 +0000

    ADD: sharpziplib para compactar MOD: migrado do 2.0

--- trunk/DotNET-C-Sharp/web-FW-1.1/App_Code/Interface/Config.cs
+++ trunk/DotNET-C-Sharp/web-FW-1.1/App_Code/Interface/Config.cs
@@ -7,39 +7,30 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
 
-namespace Interface{
-	/**
-	 * Interface.Config
-	 * @since 08/07/2008
-	 * @version 1.0
-	 * @author Regis Bittencourt - rbittencourt@fbiz.com.br * mod for 1.1, Marcelo Miranda Carneiro - mcarneiro@gmail.com 
-	 */
-	public class Config{
-	    /**
-		 * Interface.Config.Title
-		 * @return String the project title name
-		 * @usage
-				<code>
-					<%=Interface.Config.Title%> //writes " - ##PAGE_TITLE##" on the page.
-				</code>
-		 */
-		public static string Title{
-	        get{
-	            return Get("Title");
-	        }
-	    }
-
-		/**
-		 * Interface.Config.Title
-		 * @return Any string stored in Web.config
-		 * @usage
-				<code>
-					<%=Interface.Config.Get("dummy")%> //writes the value 0 from key dummy in web.config: <add key="dummy" value="0"/>
-				</code>
-		 */
-		public static string Get(string key)
-		{
-			return ConfigurationSettings.AppSettings.Get(key);
-		}
-	}
+namespace Interface
+{
+    /**
+     * Common.Config
+     * @since 08/07/2008
+     * @version 1.0
+     * @author Regis Bittencourt - rbittencourt@fbiz.com.br, Marcelo Miranda Carneiro - mcarneiro@gmail.com
+     */
+    public class Config
+    {
+        /**
+         * Common.Config.Title
+         * @return String the project title name
+         * @usage
+                <code>
+                    <%=Common.Config.Title%> //writes " - ##PAGE_TITLE##" on the page.
+                </code>
+         */
+        public static string Title
+        {
+            get
+            {
+                return " - ##PAGE_TITLE##";
+            }
+        }
+    }
 }
