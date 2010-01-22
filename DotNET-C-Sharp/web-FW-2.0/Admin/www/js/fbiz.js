c5f760b9d3e018c4d91e0b40967a0c755bc88d46
edu.tata <edu.tata@be60e662-3d4f-0410-86c1-07df980b295e>
2010-01-22 21:31:10 +0000

    ADD: Adicionadas funções js para funcionamento padrão do admin. NEW: Adicionados novos aspx para telas internas do admin.

--- DotNET-C-Sharp/web-FW-2.0/Admin/www/js/fbiz.js
+++ DotNET-C-Sharp/web-FW-2.0/Admin/www/js/fbiz.js
@@ -1,5 +1,41 @@
 (function(scope, $) {
 	
 	// namespace
+	scope.fbiz.tabs = function(cl){
+		cl = cl ? cl : '.tabs'
+		
+		/* Tabs Animation */
+		var tabs = $(cl)
+			tabs			
+			.find('div').hide().end()
+			.find('div:first').show().end()
+			.find('ul li:first').addClass('active').parent()
+			
+			.find('a').click(function(){ 
+				tabs.find('li').removeClass('active')
+				$(this).parent().addClass('active')				
+				var currentTab = $(this).attr('href')				
+				tabs.find('div').hide()
+				$(currentTab).show()
+				return false
+			})
+	}
 	
-})(window, $);
+	scope.fbiz.tablesort = function(cl){
+		cl = cl ? cl : '.tablesorter'
+		$(cl).tablesorter()
+	}
+	
+	scope.fbiz.nav = function(el){
+		var el = el ? el :  $('body').attr('class')
+		$('#nav ul').find('li a')
+			.parent().removeClass('active').end()
+			.removeClass('active').each(function(){		
+				console.log($(this).text().toLowerCase().match(el))
+				if( $(this).text().toLowerCase().match(el) )  
+					$(this).addClass('active').parent().addClass('active') 
+			})
+			
+	}  
+		
+})(window, jQuery);
