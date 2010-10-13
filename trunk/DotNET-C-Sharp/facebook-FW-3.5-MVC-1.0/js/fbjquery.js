7fde10db21d4c5b33f9eba16df81b0aac3721cab
edu.tata <edu.tata@be60e662-3d4f-0410-86c1-07df980b295e>
2010-10-13 15:31:21 +0000

    NEW: Versionando primeira versão do módulo padrão do Facebook Fan Pages.

---
+++ trunk/DotNET-C-Sharp/facebook-FW-3.5-MVC-1.0/js/fbjquery.js
@@ -0,0 +1,399 @@
+var alert = function(p_str, p_tit) {
+	var dialog = new Dialog().showMessage(p_tit || ' ', p_str);
+};
+var FbjQuery = {
+
+	// CONSTANTES
+	VERSION: '0.1',
+	ROOT: document.getRootElement(),
+	/*
+		addClassName(a), addEventListener(f, b), appendChild(a), callSWF(e), cloneNode(c), constructorfbjs_dom(c, a), focus(), getAbsoluteLeft(), getAbsoluteTop(), getAccessKey(), getAction(), getChecked(), getChildNodes(), getClassName(), getClientHeight(), getClientWidth(), getColSpan(), getCols(), getDir(a), getDisabled(), getElementsByTagName(b), getFirstChild(), getForm(), getHref(), getId(), getLastChild(), getMaxLength(), getMethod(), getName(), getNextSibling(), getNodeType(), getOffsetHeight(), getOffsetWidth(), getOptions(), getParentNode(), getPreviousSibling(), getReadOnly(), getRowSpan(), getRows(), getScrollHeight(), getScrollLeft(), getScrollTop(), getScrollWidth(a), getSelected(), getSelectedIndex(), getSelection(), getSrc(), getStyle(a), getTabIndex(), getTagName(), getTarget(), getTitle(), getType(), getValue(), getdir(a), hasClassName(a), insertBefore(b, a), listEventListeners(e), purgeEventListeners(e), removeChild(a), removeClassName(a), removeEventListener(f, b), replaceChild(a, b), select(), serialize(), setAccessKey(a), setAction(a), setChecked(a), setClassName(a), setColSpan(a), setCols(a), setDir(a), setDisabled(a), setHref(a), setId(b), setInnerFBML(a), setInnerXHTML(b), setMaxLength(a), setMethod(a), setName(a), setReadOnly(a), setRowSpan(a), setRows(a), setScrollLeft(a), setScrollTop(a), setSelected(a), setSelectedIndex(a), setSelection(c, a), setSrc(a), setStyle(a, b), setTabIndex(a), setTarget(a), setTextValue(b), setTitle(a), setType(a), setValue(a), submit(), toggleClassName(a)
+	*/
+	
+	// Static Methods
+	// AJAX
+	genericAjax: function(p_url, p_query, p_responsetype, p_complete, p_error, p_requirelogin, p_uselocalproxy) {
+
+		var _url = p_url;
+		var _query = p_query || null;
+		var _responseType = p_responsetype || Ajax.RAW; // Ajax.RAW, Ajax.JSON, Ajax.FBML;
+		var _complete = p_complete || function(){};
+		var _error = p_error || function(){};
+		var _requireLogin = p_requirelogin || false;
+		var _useLocalProxy = p_uselocalproxy || false;
+		var _ajax = new Ajax();
+			_ajax.responseType = _responseType;
+			_ajax.requireLogin = _requireLogin;
+			_ajax.useLocalProxy = _useLocalProxy;
+			_ajax.ondone = _complete
+			_ajax.onerror = _error
+		_ajax.post(_url, _query);
+
+	},
+	ajax: function(p_objAjax) {
+		var _objAjax = p_objAjax;
+		var _cache;
+		var _url, _query, _responseType, _complete, _error, _requirelogin, _uselocalproxy; // FB Objects
+		
+		if(FbjQuery.isObject(_objAjax)) {
+			var respType;
+			switch(_objAjax.dataType) {
+				// case 'jsonp' :
+				// case 'script' :
+				// 	throw new Error('ERROR: dataType "' + _objAjax.dataType + '" not avaliable for FbjQuery.')
+				// 	break;
+				case 'html' :
+					respType = Ajax.FBML;
+					break;
+				case 'json' :
+					respType = Ajax.JSON;
+					break;
+				default : // xml, text
+					respType = Ajax.RAW;
+					break;
+			}
+			_url = _objAjax.url;
+			_query = _objAjax.data;
+			_responseType = respType; // Ajax.RAW, Ajax.JSON, Ajax.FBML;
+			_complete = _objAjax.complete || function(){};
+			_error = _objAjax.error || function(){};
+			_requirelogin = _objAjax.requireLogin || false;
+			_uselocalproxy = _objAjax.useLocalProxy || false;
+			_cache = (_objAjax.cache != null && (_objAjax.cache === false || _objAjax.cache == 'false')) ? false : true;
+		}
+		else if(typeof(_objAjax) == 'string') _url = _objAjax;
+		
+		if(_cache === false) {
+			_url += (_url.match(/\?/) ? '&' : '?') + 'rnd=' + Math.random(10000);
+		}
+		FbjQuery.genericAjax(_url, _query, _responseType, _complete, _error, _requirelogin, _uselocalproxy);
+	},
+	
+	post: function(p_objAjax) {
+		FbjQuery.ajax(p_objAjax);
+	},
+	
+	get: function(p_objAjax) {
+		FbjQuery.ajax(p_objAjax);
+	},
+	
+	// UTIL
+	/**
+	@see http://forum.jquery.com/topic/why-use-jquery-isobject-in-jquery-extend
+	*/
+	isObject: function(p_obj){
+		// return Object.prototype.toString.call(p_obj) === '[object Object]';
+		return (typeof(p_obj) == 'object' && p_obj != null && p_obj.length == null);
+	},
+	isArray: function(p_obj){
+		// return Object.prototype.toString.call(p_obj) === '[object Array]';
+		return (typeof(p_obj) == 'object' && p_obj != null && p_obj.length != null);
+	},
+	isFunction: function(p_fn) {
+		//the following condition should be in there too, but FBJS prevents access to Array
+		//fn.constructor != [].constructor
+		return !!p_fn && typeof p_fn != 'string' && !p_fn.nodeName &&  /^[\s[]?function/.test( p_fn + '' );
+	},
+	extend: function() {
+		// copy reference to target object
+		var target = arguments[0] || {},
+			i = 1,
+			length = arguments.length,
+			deep = false,
+			options;
+
+		// Handle a deep copy situation
+		if(typeof(target) == "boolean") {
+			deep = target;
+			target = arguments[1] || {};
+			// skip the boolean and the target
+			i = 2;
+		}
+
+		// Handle case when target is a string or something (possible in deep copy)
+		if(typeof(target) != "object" && typeof(target) != "function") {
+			target = {};
+		}
+
+		// extend jQuery itself if only one argument is passed
+		if(length == i) {
+			target = this;
+			--i;
+		}
+
+		for(; i < length; i++) {
+			// Only deal with non-null/undefined values
+			if((options = arguments[i])) {
+				// Extend the base object
+				for(var name in options) {
+					var src = target[name],
+						copy = options[name];
+
+					// Prevent never-ending loop
+					if(target === copy) { continue; }
+
+					// Recurse if we're merging object values
+					if (deep && copy && typeof(copy) == "object" && !copy.getNodeType) {
+						target[name] = FBjqRY.extend(deep, // Never move original objects, clone them
+										src || (copy.length ? [] : {}), copy);
+					} else if (copy !== undefined) { // Don't bring in undefined values
+						target[name] = copy;
+					}
+				}
+			}
+		}
+		// Return the modified object
+		return target;
+	},
+	util: {
+		convertObjCSSProp: function(p_obj){
+			var tmpObj = {};
+			for(var i in p_obj) {
+				tmpObj[FbjQuery.util.transformCSSPropToJSProp(i)] = p_obj[i];
+			}
+			return tmpObj;
+		},
+		transformCSSPropToJSProp: function(p_str){ 
+			var arrText = p_str.split('-');
+			var arrConverted = [];
+				arrConverted.push(arrText[0]);
+			
+			for(var i=1, len = arrText.length; i < len; i++) {
+				var firtsLetter = arrText[i].slice(0,1);
+					firtsLetter = firtsLetter.toUpperCase();
+				var word = arrText[i].slice(1,arrText[i].length)
+				arrConverted.push(firtsLetter + word);
+			}
+			return arrConverted.join('');
+		}
+	},
+
+	// Dynamic Class
+	Class: function(p_selector){
+		// private methods FIRST
+		var _applyFBJS = function(p_func, p_args, p_context) {
+			var _context = p_context || _root;
+			for(var i = 0; i < _$len; i++) {
+				_context[p_func].apply(_$[i], p_args);
+			}
+		}
+
+		// Selectors
+		var _getById = function(p_id) {
+			return [document.getElementById(p_id)];
+		};
+
+		var _getByTag = function(p_tag) {
+			return _root.getElementsByTagName(p_tag || '*');
+		};
+
+		var _getByClass = function(p_class, p_context) {
+			var _arrTags = _getByTag(p_context);
+			var _arrHasClass = [];
+
+			for(var i = 0, len = _arrTags.length; i < len; i++) {
+				var _elm = _arrTags[i];
+
+				if(_arrTags[i].hasClassName(p_class)) {
+					_arrHasClass[_arrHasClass.length] = _arrTags[i];
+				}
+			}
+
+			return _arrHasClass;
+		};	
+
+		// Getting the OBJ!
+		var _$;
+		var _$len;
+		var _root = FbjQuery.ROOT;
+		var _this = this;
+		
+		if(FbjQuery.isObject(p_selector)) { // Já é um objeto DOM
+			_$ = [p_selector];
+		} else {
+			var selector = p_selector.slice(0,1);
+			_$ = null;
+			_$len = 0;
+			
+			// constructor
+			if(selector == '#') {
+				_$ = _getById(p_selector.replace('#',''));
+			} else if(selector == '.') {
+				_$ = _getByClass(p_selector.replace('.',''));
+			} else {
+				_$ = _getByTag(p_selector);
+			}
+			
+		}
+		this.length = _$len = _$.length;
+
+		// public methods returning "this"
+		this.hide = function() {
+			_applyFBJS('setStyle', ['display', 'none']);
+			return this;
+		};
+
+		this.show = function() {
+			_applyFBJS('setStyle', ['display', 'block']);
+			return this;
+		};
+
+		this.text = function(p_text) {
+			_applyFBJS('setTextValue', [p_text]);
+			return this;
+		};
+
+		this.fbml = function(p_html) {
+			_applyFBJS('setInnerFBML', [p_html], document);
+			return this;
+		};
+
+		this.html = function(p_html) {
+			_applyFBJS('setInnerXHTML', [p_html]);
+			return this;
+		};
+
+		this.css = function(p_prop, p_value) {
+			var _objStyle = {};
+			
+			if(arguments.length == 0 ) {
+				return this;
+			} else if(arguments.length == 1 ) { // Getter ou passado por Objeto
+			 
+				if(FbjQuery.isObject(p_prop)) { // Setter por objeto
+					_objStyle = FbjQuery.util.convertObjCSSProp(p_prop);
+				} else if(p_prop == 'string'){ // Getter
+					return _applyFBJS('getStyle', [FbjQuery.util.transformCSSPropToJSProp(p_prop)]);
+				}
+				
+			} else if(arguments.length == 2 ){ // Setter por "prop":"valor"
+				_objStyle[FbjQuery.util.transformCSSPropToJSProp(p_prop)] = p_value;
+			}
+			
+			_applyFBJS('setStyle', [_objStyle]);
+			
+			return this;
+		};
+		
+		this.espirro = function(p_objAjax) {
+			var tmpObj = {
+				dataType:'html',
+				error: function() {
+					alert('ERRO!')
+				},
+				complete: function(response){
+					_this.fbml(response);
+				}
+			};
+			var newObj = FbjQuery.extend(p_objAjax, tmpObj);
+			FbjQuery.ajax(newObj);
+			return this;
+		};
+		
+
+		// Métodos Ajax que funcionam exatamente igual
+		var _ajaxArr = ['ajax','post','get'];
+		for(var i = 0; _ajaxArr[i]; i++) {
+			(function() {
+				var _ajaxName = _ajaxArr[i];
+				_this[_ajaxName] = function(p_objAjax) {
+					FbjQuery.ajax(p_objAjax);
+					return _this;
+				};
+			})();
+		};
+		_ajaxArr = null;
+	
+		this.bind = function(p_event, p_handler, p_usecapture) {
+			_applyFBJS('addEventListener', [p_event, p_handler, p_usecapture]);
+			return this;
+		};
+		
+		// Fazendo bind de todos os eventos possíveis!
+		// TODO: testar quais realmente funcionam!
+		// var _eventsArr = ['blur','focus','focusin','focusout','load','resize','scroll','unload','click','dblclick','mousedown','mouseup','mousemove','mouseover','mouseout','mouseenter','mouseleave','change','select','submit','keydown','keypress','keyup','error'];
+		// for(var i = 0; _eventsArr[i]; i++) {
+		// 	(function() {
+		// 		var _eventName = _eventsArr[i];
+		// 		_this[_eventName] = function(p_handler, p_usecapture) {
+		// 			return _this.bind(_eventName, p_handler, p_usecapture)
+		// 		};
+		// 	})();
+		// };
+		// _eventsArr = null;
+	}
+
+};
+var pageTracker = {
+	PAGE_VIEW: 'trackPageview',
+	PAGE_EVENT: 'trackEvent',
+	UACode: null,
+	URLProxy: null,
+	domain: 'facebook.com',
+	title: '',
+	_testRequiredParameters: function() {
+		if(!pageTracker.UACode) {
+			alert('[pageTracker.' + p_obj.func + '] pageTracker.UACode is undefined;', 'ERROR');
+			return false;
+		}
+		if(!pageTracker.URLProxy) {
+			alert('[pageTracker.' + p_obj.func + '] pageTracker.URLProxy is undefined;', 'ERROR');
+			return false;
+		}
+		if(!pageTracker.domain) {
+			alert('[pageTracker.' + p_obj.func + '] pageTracker.domain is undefined;', 'ERROR');
+			return false;
+		}	
+	},
+	_trackPageview: function (p_str){
+		
+		var tmpObj = {
+			url: pageTracker.URLProxy,
+			cache: false,
+			data: {
+				'googlecode': 		pageTracker.UACode,
+				'googledomain': 	pageTracker.domain,
+				
+				'pagetitle': 		pageTracker.title,
+				'pagelink': 		p_str,
+				
+				'func': 			pageTracker.PAGE_VIEW
+			}
+		};
+		
+		pageTracker._testRequiredParameters(tmpObj.data);
+		
+		FbjQuery.ajax(tmpObj);
+	},
+	jujuba: function (p_category, p_action, p_label, p_value){
+		
+		var tmpObj = {
+			url: pageTracker.URLProxy,
+			cache: false,
+			data: {
+				'googlecode': 		pageTracker.UACode,
+				'googledomain': 	pageTracker.domain,
+				
+				'category': 		p_category || '',
+				'action': 			p_action || '',
+				'label': 			p_label || '',
+				'value': 			p_value || '',
+				
+				'func': 			pageTracker.PAGE_VIEW
+			}
+		};
+		
+		pageTracker._testRequiredParameters(tmpObj.data);
+		
+		FbjQuery.ajax(tmpObj);
+		
+	},
+	_setVar: function (w){
+	}
+	
+};
+
+var $ = function(p_selector) {
+	return new FbjQuery.Class(p_selector);
+};
+FbjQuery.extend($, FbjQuery, {Class:null});
