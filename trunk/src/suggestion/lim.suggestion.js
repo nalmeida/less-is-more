4a103a536262bba8ebb4e087ca62ca8506186bfd
edu.tata <edu.tata@be60e662-3d4f-0410-86c1-07df980b295e>
2011-02-21 18:29:09 +0000

    MOD: Alterada estrutura de chamada da classe para diminuir a programação final e documentado.

--- trunk/src/suggestion/lim.suggestion.js
+++ trunk/src/suggestion/lim.suggestion.js
@@ -1,14 +1,74 @@
+/**
+ * @class Suggestion
+ * @author Eduardo Ottaviani
+ * @usage 
+	var suggestion = new lim.Suggestion({
+		target	:'#my-input-text',
+		list	:'#list',
+		
+		url		: 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?',
+		params	:{
+			tags: "cat",
+			tagmode: "any",
+			format: "json"
+		},
+		
+		change	:'tags',
+		template:'<li><img src="{image}" /><a href="{link}">{title}</a></li>',
+		array :'items',
+		
+		merge	:{
+			image :'media.m',
+			link  :'link',
+			title :'title'
+		}
+	});
+	
+	Events:
+		//On data loaded
+		 $(suggestion).bind('load', function(e, data){
+			console.log(data);
+		})
+*/
+
 ;(function(namespace, $){
 	
-	var Sugestion = {
+	var Mixin = {
+		
+		to_object :function(s, o){
+			var o = Mixin.dup(o);
+			var split = s.split(/\./);
+				if( split.length > 1 )
+					for(var i = 0; i < split.length; i++){
+						o = o[split[i]];
+					}
+				else
+					o = o[split[0]];
+				
+			return o;
+		},
+		
+		dup	:function(o){
+			var f = function(){};
+				f.prototype = o;
+			return new f;
+		}
+		
+	}
+
+	var Suggestion = {
 		
 		Class :function( options ){
 
 			//Private
-			var pattern =  /\{(\w*)\}/g;
-			var template = options.template;
-			var timer = null;
-			var self = this;
+			var 
+				pattern =  /\{(\w*)\}/g,
+				template = options.template,
+				timer = null,
+				self = this,
+				to_object = Mixin.to_object,
+				create	= Mixin.dup,
+				list = $(options.list);
 
 			//Public
 			this.timeout = options.timeout || 500;
@@ -21,8 +81,10 @@
 				return html;
 			}
 			
-			this.get = function(options){
-				$.getJSON( options.url, options.params, function(data){ options.callback(data); });
+			this.get = function(){
+				$.getJSON( options.url, options.params, function(data){
+					self.callback.call( self, data );
+				});
 			}
 
 			this.request = function(value){
@@ -30,7 +92,7 @@
 				var self = this;
 				timer = setTimeout(function(){
 					options.params[options.change] = self.query = value;
-					self.get(options);
+					self.get();
 				}, this.timeout);
 			}
 
@@ -40,6 +102,27 @@
 					if(this.value)
 						self.request( this.value ); 
 			}
+			
+			this.callback = options.callback || function(data){
+
+				$(this).trigger('load', data)
+
+				var 
+					merge = options.merge,
+					array = to_object( options.array, data ),
+					html = '',
+					o = {};
+					
+						for(var i = 0; i < array.length; i++){
+								for( var key in merge ){
+									o[key] = to_object( merge[key], array[i] ); 
+								}
+							html += this.inject( o );
+						}
+				
+				list.empty().append( html );
+				
+			}
 
 			//Constructor
 			;(function(){
@@ -48,6 +131,6 @@
 		}
 	}
 
-	namespace.Sugestion = Sugestion.Class;
+	namespace.Suggestion = Suggestion.Class;
 
 })(lim, jQuery)
