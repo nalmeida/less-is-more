959ed029c1a095092e6a44927801e86552b97a5e
wmsbill@gmail.com <wmsbill@gmail.com@be60e662-3d4f-0410-86c1-07df980b295e>
2011-06-17 18:22:56 +0000

    DEL: c\363digo indevido de debug

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -58,24 +58,7 @@ class Controller{
 	protected function _parseView($content){
 		$this->viewDictionary = array();
 		preg_replace_callback(self::regexTag, array(&$this, "_createContent"), $content);
-		switch(preg_last_error()){
-			case PREG_INTERNAL_ERROR:
-				echo "PREG_INTERNAL_ERROR";
-			break;
-			case PREG_BACKTRACK_LIMIT_ERROR:
-				echo "PREG_BACKTRACK_LIMIT_ERROR";
-			break;
-			case PREG_RECURSION_LIMIT_ERROR:
-				echo "PREG_RECURSION_LIMIT_ERROR";
-			break;
-			case PREG_BAD_UTF8_ERROR:
-				echo "PREG_BAD_UTF8_ERROR";
-			break;
-			case PREG_BAD_UTF8_OFFSET_ERROR:
-				echo "PREG_BAD_UTF8_OFFSET_ERROR";
-			break;
-			default:;
-		}
+	
 	}
 	
 	protected function _parseMasterPage($content){
